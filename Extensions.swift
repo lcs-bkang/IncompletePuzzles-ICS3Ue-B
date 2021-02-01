abd4df88ff42c9daea9dab11da998cc6eb7f2532
Bradley Kang <bkang@lcs.on.ca>
2021-02-01 11:57:42 -0500

    Added extensions function to be used for doubles

--- Extensions.swift
+++ Extensions.swift
@@ -14,7 +14,7 @@ extension Int {
     // Prompt - The text that will be shown to the user.
     // Minimum - If there is a minimum value, give it here.
     // Maximum - If there is a maximum value, give it here.
-    static func collectInput(withPrompt prompt: String, minimum: Int?, maximum: Int?) -> Int {
+    static func collectInputInt(withPrompt prompt: String, minimum: Int?, maximum: Int?) -> Int {
         
         // Loop until a valid value is provided
         while true {
@@ -61,3 +61,53 @@ extension Int {
     }
     
 }
+
+extension Double {
+    
+    static func collectInputDouble(withPrompt prompt: String, minimum: Int?, maximum: Int?) -> Int {
+        
+        // Loop until a valid value is provided
+        while true {
+            
+            // Prompt the user
+            print(prompt)
+            
+            // Collect the input
+            guard let givenInput = readLine() else {
+                continue
+            }
+            
+            // Convert to an integer
+            guard let givenInteger = Int(givenInput) else {
+                continue
+            }
+            
+            // If a lowest value for the integer was specified...
+            if let minimumValue = minimum {
+                
+                // ... then check that the given integer is greater than or equal to the lowest desired value.
+                guard givenInteger >= minimumValue else {
+                    continue
+                }
+                
+            }
+            
+            // If an highest possible value for the integer was specified...
+            if let maximumValue = maximum {
+                
+                // ... then check that the given integer is less than or equal to the highest desired value.
+                guard givenInteger <= maximumValue else {
+                    continue
+                }
+                
+                
+            }
+            
+            // If we've made it past all the checks, the input is an integer in the desired range of values, so, return it
+            return givenInteger
+            
+        }
+        
+    }
+}
+
